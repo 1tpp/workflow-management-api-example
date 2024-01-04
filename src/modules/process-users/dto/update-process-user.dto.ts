0746cf7027417d5536eeccf53400e1b26cedb31a
1tpp <panapat.dev@gmail.com>
2024-01-04 18:37:46 +0700

    Delete process-users module and related files

--- src/modules/process-users/dto/update-process-user.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateProcessUserDto } from './create-process-user.dto';
-
-export class UpdateProcessUserDto extends PartialType(CreateProcessUserDto) {}
