50e0457a65752ef472d45c1d84e5646223ce1947
1tpp <panapat.dev@gmail.com>
2024-01-04 18:03:12 +0700

    Add configuration file and update database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
+import configuration from './config/configuration';
+import { ConfigService } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -16,16 +20,37 @@ import { RequestStakeHoldersModule } from './modules/request-stake-holders/reque
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      password: 'simform',
-      username: 'postgres',
-      entities: [],
-      database: 'pgWithNest',
-      synchronize: true,
-      logging: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: configuration,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const dbConfig = {
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PORT'),
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_NAME'),
+        };
+
+        console.log(dbConfig);
+
+        return {
+          type: 'postgres',
+          host: dbConfig.host,
+          port: dbConfig.port,
+          username: dbConfig.username,
+          password: dbConfig.password,
+          database: dbConfig.database,
+          entities: [__dirname + '/**/*.entity{.ts,.js}'],
+          synchronize: true, // TODO: change to false in production
+          logging: true, // TODO: change to false in production
+          ssl: false,
+        };
+      },
+      inject: [ConfigService],
     }),
     UsersModule,
     ProcessesModule,
---
+++ src/config/configuration.ts
@@ -0,0 +1,15 @@
+import * as Joi from 'joi';
+
+export default Joi.object({
+  NODE_ENV: Joi.string()
+    .valid('development', 'production', 'test', 'provision')
+    .default('development'),
+  PORT: Joi.number().required(),
+  DB_HOST: Joi.string().required(),
+  DB_PORT: Joi.number().required(),
+  DB_NAME: Joi.string().required(),
+  DB_USERNAME: Joi.string().required(),
+  DB_PASSWORD: Joi.string().required(),
+  JWT_ACCESS_SECRET_KEY: Joi.string().required(),
+  JWT_ACCESS_EXPIRES_IN: Joi.string().required(),
+});
